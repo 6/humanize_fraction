a16acc6c57ccdce6a2ca166c9dea81ffaaafda11
Peter Graham <peterghm@gmail.com>
2017-07-08 16:47:55 -0400

    basic handling of fractions

--- lib/fraction_to_words.rb
+++ lib/fraction_to_words.rb
@@ -1,5 +1,57 @@
+require "active_support/inflector"
+require "humanize"
+require "twitter_cldr"
+
 require "fraction_to_words/version"
 
-module FractionToWords
-  # Your code goes here...
+class FractionToWords
+  attr_reader :numerator, :denominator, :options
+
+  def initialize(numerator, denominator, options = {})
+    [numerator, denominator].each do |number|
+      if !number.is_a?(Integer)
+        raise ArgumentError, "Expected Integer but got #{number.class.name}"
+      end
+    end
+    @numerator = numerator
+    @denominator = denominator
+    @options = options
+  end
+
+  def to_s
+    words = []
+    words << humanize_numerator
+    words << humanize_denominator
+    words.join(" ")
+  end
+
+  private
+
+  def humanize_numerator
+    number = if numerator == 1 && !options[:always_spell_out]
+      first_digit(denominator) == 8 ? "an" : "a"
+    else
+      numerator.humanize
+    end
+    number
+  end
+
+  def humanize_denominator
+    number = case denominator
+    when 2
+      "half"
+    when 4
+      "quarter"
+    else
+      denominator.localize(:en).to_rbnf_s("SpelloutRules", "spellout-ordinal")
+    end
+    if numerator != 1
+      number = ActiveSupport::Inflector.pluralize(number)
+    end
+    number
+  end
+
+  def first_digit(number)
+    number.to_s.split("").first.to_i
+  end
 end
--- spec/fraction_to_words_spec.rb
+++ spec/fraction_to_words_spec.rb
@@ -1,11 +1,26 @@
-require "spec_helper"
-
-RSpec.describe FractionToWords do
+describe FractionToWords do
   it "has a version number" do
     expect(FractionToWords::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#to_s" do
+    [
+      [0, 2, "zero halves"],
+      [1, 2, "a half"],
+      [2, 2, "two halves"],
+      [0, 3, "zero thirds"],
+      [1, 3, "a third"],
+      [2, 3, "two thirds"],
+      [3, 3, "three thirds"],
+      [0, 8, "zero eighths"],
+      [1, 8, "an eighth"],
+      [2, 8, "two eighths"],
+    ].each do |numerator, denominator, expected_output|
+      context "#{numerator}/#{denominator}" do
+        subject { described_class.new(numerator, denominator).to_s }
+
+        it { is_expected.to eq(expected_output) }
+      end
+    end
   end
 end
